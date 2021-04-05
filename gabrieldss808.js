4ae87dbb3aa660b94905d56b0665c4914fc00123
Gabriel Da Silva Souza <gabriel.ssouza@totvs.com.br>
2021-04-05 18:15:44 -0300

    new tests

--- gabrieldss808.js
+++ gabrieldss808.js
@@ -11,16 +11,3 @@ function closeWindows(classText) {
 
     $("." + classText).hide(1000);
 }
-
-function alterDataJson() {
-
-    const octokit = new Octokit({ auth: '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' });
-
-    const result = octokit.request('PUT repos/gabrieldss808/gabrieldss808.github.io/contents/data/data.json', {
-        owner: 'gabrieldss808',
-        repo: 'gabrieldss808.github.io',
-        path: 'data/data.json',
-        message: 'Deu Certo',
-        content: 'content'
-    })
-}
