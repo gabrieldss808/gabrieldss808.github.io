e7137df84545c659c1e1d6875300bfcf69499829
Gabriel Da Silva Souza <gabriel.ssouza@totvs.com.br>
2021-04-05 18:06:47 -0300

    new tests

--- gabrieldss808.js
+++ gabrieldss808.js
@@ -16,7 +16,7 @@ function alterDataJson() {
 
     const octokit = new Octokit({ auth: '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' });
 
-    const result = await octokit.request('PUT repos/gabrieldss808/gabrieldss808.github.io/contents/data/data.json', {
+    const result = octokit.request('PUT repos/gabrieldss808/gabrieldss808.github.io/contents/data/data.json', {
         owner: 'gabrieldss808',
         repo: 'gabrieldss808.github.io',
         path: 'data/data.json',
