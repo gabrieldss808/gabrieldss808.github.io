6b1642b34653f14571662dd590efa7cfb19d216e
Gabriel Da Silva Souza <gabriel.ssouza@totvs.com.br>
2021-12-19 02:56:16 -0300

    New changes of my new design website

--- gabrieldss808.js
+++ gabrieldss808.js
@@ -1,54 +1,14 @@
 $(document).ready(function() {
-    //ini();
+    ini();
 });
+
+var IntervalID
 
 function ini() {
 
-    $(".logo2").show(200, function() {
-        $(".logo2").animate({
-            "left": "27%"
-        }, 1000, function() {
-
-        });
-        $(".logo3").animate({
-            "left": "73%"
-        }, 1000, function() {
-            $(".logo1").show()
-            $(".logo1").animate({
-                "width": "490px",
-                "left": "35.2%"
-            }, 1900, function() {
-                $(".logo3").show(200, function() {
-                    sleep(1000);
-                    $(".logo1").animate({
-                        "width": "0px",
-                        "left": "32%"
-                    }, 1900, function() {
-                        $(".logo4").show();
-                        $(".logo2").animate({
-                            "left": "27%"
-                        }, 1900, function() {
-
-                        });
-                        $(".logo3").animate({
-                            "left": "65%"
-                        }, 1900, function() {
-
-                        });
-                        $(".logo4").animate({
-                            "width": "363px",
-                            "left": "36%"
-                        }, 1900, function() {
-                            sleep(1000);
-                            $(".logo1").hide(700);
-                            $(".logo2").hide(700);
-                            $(".logo3").hide(700);
-                            $(".logo4").hide(700);
-                        });
-                    });
-                });
-            });
-        });
+    $(".logo1").show(600);
+    $(".logo2").show(600, function() {
+        IntervalID = setInterval(function() { afterInitialAnimate() }, 1500);
     });
 }
 
@@ -57,4 +17,15 @@ function sleep(sleepDuration) {
     while (new Date().getTime() < now + sleepDuration) {
         /* Do nothing */
     }
+}
+
+function afterInitialAnimate() {
+
+    $(".logo1").hide();
+    $(".logo2").hide();
+    $(".logoFinal").show(100, function() {
+        $(".lateralBar").show(200);
+    });
+
+    clearInterval(IntervalID)
 }
