3836e9d8ad00b0765a6f52400d3a154aa198f0fe
GABRIEL DA SILVA SOUZA <45453977+gabrieldss808@users.noreply.github.com>
2021-12-19 01:33:01 -0300

    new design of website

--- gabrieldss808.js
+++ gabrieldss808.js
@@ -1,13 +1,60 @@
-function showWindow(classText) {
+$(document).ready(function() {
+    //ini();
+});
 
-    $("." + classText).show(500)
+function ini() {
 
-    $("html, body").animate({
-        scrollTop: ($("." + classText).first().offset().top)
-    }, 2000);
+    $(".logo2").show(200, function() {
+        $(".logo2").animate({
+            "left": "27%"
+        }, 1000, function() {
+
+        });
+        $(".logo3").animate({
+            "left": "73%"
+        }, 1000, function() {
+            $(".logo1").show()
+            $(".logo1").animate({
+                "width": "490px",
+                "left": "35.2%"
+            }, 1900, function() {
+                $(".logo3").show(200, function() {
+                    sleep(1000);
+                    $(".logo1").animate({
+                        "width": "0px",
+                        "left": "32%"
+                    }, 1900, function() {
+                        $(".logo4").show();
+                        $(".logo2").animate({
+                            "left": "27%"
+                        }, 1900, function() {
+
+                        });
+                        $(".logo3").animate({
+                            "left": "65%"
+                        }, 1900, function() {
+
+                        });
+                        $(".logo4").animate({
+                            "width": "363px",
+                            "left": "36%"
+                        }, 1900, function() {
+                            sleep(1000);
+                            $(".logo1").hide(700);
+                            $(".logo2").hide(700);
+                            $(".logo3").hide(700);
+                            $(".logo4").hide(700);
+                        });
+                    });
+                });
+            });
+        });
+    });
 }
 
-function closeWindows(classText) {
-
-    $("." + classText).hide(1000);
+function sleep(sleepDuration) {
+    var now = new Date().getTime();
+    while (new Date().getTime() < now + sleepDuration) {
+        /* Do nothing */
+    }
 }
