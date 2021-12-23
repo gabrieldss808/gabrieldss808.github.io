83ac7c230665db6036c80582cf312ce95a553232
GABRIEL DA SILVA SOUZA <45453977+gabrieldss808@users.noreply.github.com>
2021-12-22 23:07:29 -0300

    New updates, and animations changes

--- gabrieldss808.js
+++ gabrieldss808.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 
-    $('.ToplateralBar').hide();
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 
@@ -8,47 +7,15 @@ $(document).ready(function() {
 
         var height = $(window).height();
         var scroll_position = $(window).scrollTop();
-        var object_position_top = scroll_position + 253
 
-        console.log(object_position_top)
+        scroollAnimations(height, scroll_position)
+    });
 
-        if (object_position_top >= (height / 2)) {
-            object_position_top = height / 2;
+    $(window).resize(function() {
+        var height = $(window).height();
+        var scroll_position = $(window).scrollTop();
 
-            $('.lateralBar').css({
-                'top': '51%',
-                'height': '49%'
-            });
-
-        } else {
-            $('.lateralBar').css({
-                'top': '39%',
-                'height': '61%'
-            });
-        }
-
-        if (object_position_top == 253) {
-            $('.ToplateralBar').hide();
-        }
-
-        if (object_position_top >= 253) {
-            $('.ToplateralBar').show();
-
-            $('.ToplateralBar').css({
-                'top': '24%'
-            });
-        }
-
-        if (object_position_top >= 317) {
-
-            $('.ToplateralBar').css({
-                'top': '7%'
-            });
-        }
-
-        $('.initialPoint').css({
-            'top': object_position_top
-        });
+        scroollAnimations(height, scroll_position)
     });
 
     ini();
@@ -59,13 +26,8 @@ var IntervalID
 function ini() {
 
     $(".logoFinal").show(1000, function() {
-        IntervalID = setInterval(function() { afterInitialAnimate() }, 500);
+        IntervalID = setInterval(function() { afterInitialAnimate() }, 100);
     });
-
-    // $(".logo1").show(600);
-    // $(".logo2").show(600, function() {
-    //     
-    // });
 }
 
 function sleep(sleepDuration) {
@@ -77,15 +39,97 @@ function sleep(sleepDuration) {
 
 function afterInitialAnimate() {
 
-    // $(".logo1").hide();
-    // $(".logo2").hide();
-    // $(".logoFinal").show(100, function() {
-    //     $(".lateralBar").show(200);
-    //     $(".initialPoint").show(200);
-    // });
-
-    $(".lateralBar").show(1200);
+    $(".lateralBar").show(1200, function() {
+        $(".initialInformations").show(600)
+        $(".ToplateralBar").show(600)
+    });
     $(".initialPoint").show(500);
 
     clearInterval(IntervalID)
+}
+
+function scroollAnimations(ScreenHeight, ScrollTop) {
+
+    animateLateralPoint(ScreenHeight, ScrollTop)
+
+    animateLateralBar(ScreenHeight, ScrollTop)
+
+}
+
+function animateLateralPoint(ScreenHeight, ScrollTop) {
+
+    var PercentScreen = ScreenHeight * 0.38
+    var object_position_top = ScrollTop + PercentScreen
+
+
+    if (object_position_top >= (ScreenHeight / 2)) {
+        object_position_top = ScreenHeight / 2;
+    }
+
+    $('.initialPoint').css({
+        'top': object_position_top
+    });
+}
+
+function animateLateralBar(ScreenHeight, ScrollTop) {
+
+    var PercentScreen = ScreenHeight * 0.39
+    var PercentHeightLtB = ScreenHeight * 0.61
+    var PercentHeightLtBT = ScreenHeight * 0.34
+    var object_position_top = ScrollTop + PercentScreen
+    var heightLateralBar = PercentHeightLtB - ScrollTop
+    var heightLateralBarTop = PercentHeightLtBT - ScrollTop
+
+    console.log(ScrollTop)
+
+    if (object_position_top >= (ScreenHeight / 2)) {
+        object_position_top = ScreenHeight / 2;
+
+        $('.lateralBar').css({
+            'top': '51%',
+            'height': '49%'
+        });
+
+        if (ScrollTop <= 174) {
+
+            heightLateralBarTop = heightLateralBarTop + 5
+
+            $('.ToplateralBar').css({
+                'top': heightLateralBarTop + "px"
+            });
+
+        } else {
+            $('.ToplateralBar').css({
+                'top': "7%"
+            });
+        }
+
+
+
+    } else {
+        $('.lateralBar').css({
+            'top': object_position_top + 'px',
+            'height': heightLateralBar + 'px'
+        });
+
+        $('.ToplateralBar').css({
+            'top': heightLateralBarTop + "px"
+        });
+    }
+
+    // if (object_position_top == 253) {
+    //     $('.ToplateralBar').hide();
+    // }
+
+    // if (object_position_top >= 253) {
+
+    // }
+
+    // if (object_position_top >= 317) {
+
+    //     $('.ToplateralBar').css({
+    //         'top': '7%'
+    //     });
+    // }
+
 }
